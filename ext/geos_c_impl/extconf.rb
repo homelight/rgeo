f157c42084b5cd1e49d9b5f1be0e0c329a14aafa
Chris Apolzon <apolzon@gmail.com>
2012-09-18 13:38:13 -0700

    new path

--- ext/geos_c_impl/extconf.rb
+++ ext/geos_c_impl/extconf.rb
@@ -44,7 +44,7 @@ else
 
   header_dirs_ =
     [
-     '/app/bin/geos/include',
+     'bin/geos/include',
      '/usr/local/include',
      '/usr/local/geos/include',
      '/opt/local/include',
@@ -56,7 +56,7 @@ else
     ]
   lib_dirs_ =
     [
-     '/app/bin/geos/lib',
+     'bin/geos/lib',
      '/usr/local/lib64',
      '/usr/local/lib',
      '/usr/local/geos/lib',
